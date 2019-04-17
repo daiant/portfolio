00465f2528e03ade70447e538dcd5bd18a6fead5
daiant <cmg2512@gmail.com>
2019-04-17 11:31:28 +0200

    hola

--- resources/js/photo_random.js
+++ resources/js/photo_random.js
@@ -1,19 +1,31 @@
 var init;
 var list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
+
 function photo() {
 var res =  Math.floor(Math.random()*list.length);
 init = res;
-var img = document.createElement("img");
-img.src = "images/fulls/"+res+".jpg";
-var src = document.getElementsByClassName('photo')[0];
-$(src).css("background-image", "url("+img.src+")");
+var src = document.getElementsByClassName('photo_changer')[0];
+// var img = document.createElement("img");
+src.src = "images/fulls/"+res+".jpg";
+// $(src).css("background-image", "url("+img.src+")");
 }
-function change_photo() {
+function decrease_photo() {
+  var aux = init-1;
+  if(aux<0) {
+    aux = list.length-1;
+  }
+  change_photo(Math.abs(aux));
+}
+function increase_photo() {
+  var aux = init+1;
+  change_photo(aux);
+}
+function change_photo(aux) {
     // var img = new Image();
-    var img = document.createElement("img");
-    var src = document.getElementsByClassName('photo')[0];
-    var res = (init + 1) % list.length;
-    img.src = "images/fulls/"+res+".jpg";
-    $(src).css("background-image", "url("+img.src+")");
+    // var img = document.createElement("img");
+    var src = document.getElementsByClassName('photo_changer')[0];
+    var res = aux % list.length;
+    src.src = "images/fulls/"+res+".jpg";
+    // $(src).css("background-image", "url("+img.src+")");
     init=res; // update counter
 }
