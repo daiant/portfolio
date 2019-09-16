de6da4d8db2e745c0472c0bafba2407cd08b5808
Carlos <cmg2512@gmail.com>
2019-09-16 14:00:38 +0200

    adfsaad
    f

--- resources/js/photo_random.js
+++ resources/js/photo_random.js
@@ -1,8 +1,9 @@
 var init;
-var list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
+var number_photos = 27;
+var list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
 
 function photo() {
-var res =  Math.floor(Math.random()*list.length);
+var res =  Math.floor(Math.random()*number_photos);
 init = res;
 var src = document.getElementsByClassName('photo_changer')[0];
 // var img = document.createElement("img");
